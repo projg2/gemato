123defb4a17b5c655979805147836f54c7aadaa3
Michał Górny <mgorny@gentoo.org>
2017-11-26 09:29:17 +0100

    Switch to setuptools and specify the deps

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # (C) 2017 Michał Górny <mgorny@gentoo.org>
 # Licensed under the terms of 2-clause BSD license
 
-from distutils.core import setup
+from setuptools import setup
 
 
 setup(
@@ -12,6 +12,13 @@ setup(
     author='Michał Górny',
     author_email='mgorny@gentoo.org',
     url='http://github.com/mgorny/gemato',
+
+    extras_require={
+        'blake2': ['pyblake2;python_version<"3.6"'],
+        'bz2': ['bz2file;python_version<"3.0"'],
+        'lzma': ['backports.lzma;python_version<"3.0"'],
+        'sha3': ['pysha3;python_version<"3.6"'],
+    },
 
     packages=['gemato'],
     scripts=['bin/gemato'],
