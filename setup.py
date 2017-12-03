02aad83177bbee3c717abe186a89c4aed24067c3
Michał Górny <mgorny@gentoo.org>
2017-12-03 11:10:42 +0100

    Bump to 9.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='gemato',
-    version='9.0',
+    version='9.1',
     description='Gentoo Manifest Tool -- a stand-alone utility to verify and update Gentoo Manifest files',
 
     author='Michał Górny',
