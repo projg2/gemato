f229c4df74b6fb54f79d81a9d3cefe66669e0cfd
Michał Górny <mgorny@gentoo.org>
2018-03-16 12:30:47 +0100

    Bump to v12.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='gemato',
-    version='12.0',
+    version='12.1',
     description='Gentoo Manifest Tool -- a stand-alone utility to verify and update Gentoo Manifest files',
 
     author='Michał Górny',
