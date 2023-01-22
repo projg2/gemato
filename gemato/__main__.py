dba80fc6b1bb026637a85eea31cfcc5ab4a3344c
Michał Górny <mgorny@gentoo.org>
2023-01-22 20:20:55 +0100

    Add a __main__ module to call the package conveniently
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

---
+++ gemato/__main__.py
@@ -0,0 +1,9 @@
+# gemato: __main__ wrapper
+# (c) 2023 Michał Górny
+# Licensed under the terms of 2-clause BSD license
+
+from gemato.cli import setuptools_main
+
+
+if __name__ == '__main__':
+    setuptools_main()
